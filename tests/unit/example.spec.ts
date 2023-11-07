98c846994b59b458d6a86a8740bffe7392906f91
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-11-07 18:15:43 +0500

    remove msg

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,15 +1,6 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+
 import { minus } from "@/helpers/minus/minus";
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+
 
 describe("minus", function () {
   test("positive", () => {
