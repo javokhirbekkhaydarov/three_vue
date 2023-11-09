963e00bc74abc801416f449fde3477311d2905f1
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2023-11-09 18:59:27 +0500

    fix plus

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>    <div class="links">
-  <p style="color: white">{{plus(5 , 5)}}</p>
+
   <router-link to="/">home</router-link>
   <router-link to="/draw">draw</router-link>
 </div>
@@ -7,7 +7,6 @@
 </template>
 
 <script setup lang="ts">
-import { plus } from "@/helpers/plus/plus";
 
 </script>
 <style>
